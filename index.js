7d2ca8e6d2b0a154cbab3f2620eab5d4fbdc54ae
Parashuram <code@r.nparashuram.com>
2013-11-03 06:54:35 -0800

    Added module to record videos

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ var app = express();
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/viewer', express.static(__dirname + '/viewer'));
+app.use('/recorded_videos', express.static(__dirname + '/recorded_videos'));
 app.use(express.static(__dirname + '/app'));
+
+app.use(express.bodyParser());
+
+var recFolder = __dirname + '/recorded_videos';
+var fs = require('fs');
+if (!fs.existsSync(recFolder)) {
+	fs.mkdirSync(recFolder)
+	fs.writeFileSync(recFolder + '/recorded.json', '[]');
+}
+
+app.post('/uploadVideo', function(req, resp) {
+	fs.createReadStream(req.files.video.path).pipe(fs.createWriteStream(recFolder + '/' + req.files.video.name));
+
+	var data = JSON.parse(fs.readFileSync(recFolder + '/recorded.json'));
+	data.push({
+		loc: '/recorded_videos/' + req.files.video.name
+	});
+	fs.writeFileSync(recFolder + '/recorded.json', JSON.stringify(data));
+});
 
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
@@ -44,3 +64,5 @@ io.sockets.on('connection', function(socket) {
 });
 
 server.listen(3000);
+app.use(express.bodyParser());
+app.use(express.bodyParser());
