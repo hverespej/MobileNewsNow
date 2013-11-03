9d6fc42ce9138c7e6605debd8b925f62ace6f5fc
Parashuram <code@r.nparashuram.com>
2013-11-03 06:11:54 -0800

    Added pause and play

--- app/scripts/reporter.js
+++ app/scripts/reporter.js
@@ -129,18 +129,44 @@ function handleUserMediaError(error) {
 	console.log('navigator.getUserMedia error: ', error);
 }
 
-var constraints = {
-	video: {
-		mandatory: {
-			maxWidth: 850,
-			maxHeight: 480
-		}
-	},
-	audio: true
-};
 
-navigator.getUserMedia(constraints, handleUserMedia, handleUserMediaError);
-console.log('Getting user media with constraints', constraints);
+MediaStreamTrack.getSources(function(sourceInfos) {
+	var audioSource = null;
+	var videoSource = null;
+
+	for (var i = 0; i != sourceInfos.length; ++i) {
+		var sourceInfo = sourceInfos[i];
+		if (sourceInfo.kind === 'audio') {
+			console.log(sourceInfo.id, sourceInfo.label || 'microphone');
+			audioSource = sourceInfo.id;
+		} else if (sourceInfo.kind === 'video') {
+			console.log(sourceInfo.id, sourceInfo.label || 'camera');
+
+			videoSource = sourceInfo.id;
+		} else {
+			console.log('Some other kind of source: ', sourceInfo);
+		}
+	}
+
+	var constraints = {
+		audio: {
+			optional: [{
+				sourceId: audioSource
+			}]
+		},
+		video: {
+			optional: [{
+				sourceId: videoSource
+			}],
+			mandatory: {
+				maxWidth: 850,
+				maxHeight: 480
+			}
+		}
+	};
+	console.log('Getting user media with constraints', constraints);
+	navigator.getUserMedia(constraints, handleUserMedia, handleUserMediaError);
+});
 
 //requestTurn('https://computeengineondemand.appspot.com/turn?username=41784574&key=4080218913');
 
