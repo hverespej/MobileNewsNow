3d34cc9fe7bac4d741bd3348e5087e1a7bb7cf14
Parashuram <code@r.nparashuram.com>
2013-11-02 17:58:09 -0700

    Added video to report page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 
-app.use(express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/app'));
 
 var server = require('http').createServer(app);
